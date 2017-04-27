3e54fec153f032b9947542fdad01d7114d6ab1d5
laura <laureen.pelaez.isaza@gmail.com>
2017-04-27 13:31:12 -0300

    adicion constructor2

--- main/javascript/javascript.js
+++ main/javascript/javascript.js
@@ -1,102 +1,89 @@
-var resultado =   [
+var resultado = [
   {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
+    'userId': 1,
+    'id': 1,
+    'title': 'delectus aut autem',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
+    'userId': 1,
+    'id': 2,
+    'title': 'quis ut nam facilis et officia qui',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
+    'userId': 1,
+    'id': 3,
+    'title': 'fugiat veniam minus',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
+    'userId': 1,
+    'id': 4,
+    'title': 'et porro tempora',
+    'completed': true
   },
   {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
+    'userId': 1,
+    'id': 5,
+    'title': 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
+    'userId': 1,
+    'id': 6,
+    'title': 'qui ullam ratione quibusdam voluptatem quia omnis',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
+    'userId': 1,
+    'id': 7,
+    'title': 'illo expedita consequatur quia in',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
+    'userId': 1,
+    'id': 8,
+    'title': 'quo adipisci enim quam ut ab',
+    'completed': true
   },
   {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
+    'userId': 1,
+    'id': 9,
+    'title': 'molestiae perspiciatis ipsa',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
+    'userId': 1,
+    'id': 10,
+    'title': 'illo est ratione doloremque quia maiores aut',
+    'completed': true
   }
-] 
+]
 
-
-
-
-
-resultado.forEach(function(el) {
-   
-
-    document.write( "<li>" +"<strong>" + "Titulo : " + "</strong>" + el.title + "</li>")
+resultado.forEach(function (el) {
+  document.write('<li>' + '<strong>' + 'Titulo : ' + '</strong>' + el.title + '</li>')
 })
 
+function ingreseTarea () {
+  var input = document.getElementById('tarea')
+  var contenedor = document.getElementsByClassName('i-list')
+  contenedor[0].innerHTML += '<li>' + input.value + '<br>'
+  input.value = ' '
 
-function ingreseTarea(){
-    var input = document.getElementById("tarea");
-
-   var contenedor = document.getElementsByClassName("i-list");
-
-   contenedor[0].innerHTML += "<li>"+ input.value + "<br>";
-
-   input.value = " ";
-   
-
-}
-
-function newTitle(userId, id, title, completed){
+  function newTitle (userId, id, title, completed) {
     this.userId = userId,
     this.id = id,
     this.title = title,
     this.completed = completed,
-    this.ingreseDato = function(){
-        var nuevoTitulo = this.title;
-        resultado.push(resultado);
+    this.ingreseDato = function () {
+      var nuevoTitulo = this.title
+      resultado.push(resultado)
     }
+  }
+
+  var nuevoIngreso = new newTitle('title')
+  nuevoIngreso.ingreseDato()
 }
 
-var nuevoIngreso = new newTitle("title");
-    nuevoIngreso.ingreseDato();
-
-
-console.log(resultado);
+console.log(resultado)
