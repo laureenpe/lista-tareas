5853d61fdf6a337e864bf4659399c8c6281bf5b5
Laura P <laureen.pelaez.isaza@gmail.com>
2017-04-27 14:02:07 -0300

    Update javascript.js

--- main/javascript/javascript.js
+++ main/javascript/javascript.js
@@ -1,74 +1,90 @@
-var res =  {
-    "search_results": [
+var resultado = [
   {
-    "userId": 1,
-    "id": 1,
-    "title": "delectus aut autem",
-    "completed": false
+    'userId': 1,
+    'id': 1,
+    'title': 'delectus aut autem',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 2,
-    "title": "quis ut nam facilis et officia qui",
-    "completed": false
+    'userId': 1,
+    'id': 2,
+    'title': 'quis ut nam facilis et officia qui',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 3,
-    "title": "fugiat veniam minus",
-    "completed": false
+    'userId': 1,
+    'id': 3,
+    'title': 'fugiat veniam minus',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 4,
-    "title": "et porro tempora",
-    "completed": true
+    'userId': 1,
+    'id': 4,
+    'title': 'et porro tempora',
+    'completed': true
   },
   {
-    "userId": 1,
-    "id": 5,
-    "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
-    "completed": false
+    'userId': 1,
+    'id': 5,
+    'title': 'laboriosam mollitia et enim quasi adipisci quia provident illum',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 6,
-    "title": "qui ullam ratione quibusdam voluptatem quia omnis",
-    "completed": false
+    'userId': 1,
+    'id': 6,
+    'title': 'qui ullam ratione quibusdam voluptatem quia omnis',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 7,
-    "title": "illo expedita consequatur quia in",
-    "completed": false
+    'userId': 1,
+    'id': 7,
+    'title': 'illo expedita consequatur quia in',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 8,
-    "title": "quo adipisci enim quam ut ab",
-    "completed": true
+    'userId': 1,
+    'id': 8,
+    'title': 'quo adipisci enim quam ut ab',
+    'completed': true
   },
   {
-    "userId": 1,
-    "id": 9,
-    "title": "molestiae perspiciatis ipsa",
-    "completed": false
+    'userId': 1,
+    'id': 9,
+    'title': 'molestiae perspiciatis ipsa',
+    'completed': false
   },
   {
-    "userId": 1,
-    "id": 10,
-    "title": "illo est ratione doloremque quia maiores aut",
-    "completed": true
+    'userId': 1,
+    'id': 10,
+    'title': 'illo est ratione doloremque quia maiores aut',
+    'completed': true
   }
-] 
-}
+]
 
-
-
-
-res.search_results.forEach(function(el) {
-   
-
-    document.write( "<li>" + el.title + "</li>")
+resultado.forEach(function (el) {
+  document.write('<li>' + '<strong>' + 'Titulo : ' + '</strong>' + el.title + '</li>')
 })
 
+function ingreseTarea () {
+  var input = document.getElementById('tarea')
+  var contenedor = document.getElementsByClassName('i-list')
+  contenedor[0].innerHTML += '<li>' + input.value + '<br>'
+  input.value = ' '
+
+  function newTitle (userId, id, title, completed) {
+    this.userId = userId,
+    this.id = id,
+    this.title = title,
+    this.completed = completed,
+    this.ingreseDato = function () {
+      var nuevoTitulo = this.title
+      resultado.push(this)
+    }
+  }
+
+  var nuevoIngreso = new newTitle('title')
+  nuevoIngreso.ingreseDato()
+  javascript: document.body.contentEditable = 'true'; document.designMode = 'on'; void 0
+}
+
+console.log(resultado)
