ca5042f73935218750dec4a8a0c30207d17995fb
Laura P <laureen.pelaez.isaza@gmail.com>
2017-04-27 14:03:56 -0300

    Delete javascript2.js

--- main/javascript/javascript2.js
+++
@@ -1,9 +0,0 @@
-function ingreseTarea(){
-    var input = document.getElementById("tarea");
-
-   var contRut = document.getElementsByClassName("i-list");
-
-   contRut[0].innerHTML += input.value +"<br>";
-
-   input.value = "";
-}
