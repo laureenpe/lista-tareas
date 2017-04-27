7f8d1aa0857bf88129b685f409469dba88b3efe7
laura <laureen.pelaez.isaza@gmail.com>
2017-04-27 11:40:42 -0300

    funcion javascript para el boton

--- main/javascript/javascript2.js
+++ main/javascript/javascript2.js
@@ -1,4 +1,9 @@
-function newElement() {
+function ingreseTarea(){
+    var input = document.getElementById("tarea");
+
+   var contRut = document.getElementsByClassName("i-list");
+
+   contRut[0].innerHTML += input.value +"<br>";
+
+   input.value = "";
 }
-
-
